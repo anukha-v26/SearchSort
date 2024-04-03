cea1a091a9248fb95736bfa5bccaaaf5636335c4
anukha-v26 <26avusirikala@castilleja.org>
2024-04-02 19:14:48 -0700

    second commit

--- src/Main.java
+++ src/Main.java
@@ -2,78 +2,204 @@ import processing.core.PApplet;
 import processing.data.Table;
 import processing.data.TableRow;
 import java.util.ArrayList;
-public class Main extends PApplet{
+public class Main extends PApplet {
     ArrayList<Song> data = new ArrayList<Song>();
     private int bottom;
     private int top;
-    private int x = 40;
-    private int y = 40;
-    private int size = 80;
+    private int middle;
+    private int x = 15;
+    private int y = 20;
     private int index = 0;
-    private Song target;
-    public static void main(String[] args) {PApplet.main("Main");}
-    public static Main app;
-    public Main(){app = this;}
-    public void settings(){
-        size(640, 520);
+    private  Song target;
+    private String input = "";
+    private boolean found = false;
+    private boolean notExist = false;
+    private boolean notYet = false;
+
+    public static void main(String[] args) {
+        PApplet.main("Main");
     }
-    public void draw(){}
+
+    public static Main app;
+
+    public Main() {
+        app = this;
+    }
+
+    public void settings() {
+        size(1450, 400);
+    }
+
+
+
     public void setup() {
         background(255);
         Table table = loadTable("data/songData.csv", "header");
         for (TableRow row : table.rows()) {
-            int iD = row.getInt("iD"); // obtain iD
+            int iD =  row.getInt("iD"); // obtain iD
+            //int iD = row.getInt("iD"); // obtain iD
             String songName = row.getString("Name"); // obtain song name
             String artist = row.getString("Artist"); // obtain artist
             int streams = row.getInt("streams"); // obtain artist
-            data.add(new Song(iD, songName, artist, streams, x, y, size, index, 0));
+            data.add(new Song(iD, songName, artist, streams, x, y, 255));
+            x+=110;
         }
-       /*
-        for (int row = 0; row < cells.length; row++) {
-            for (int col = 0; col < cells[0].length; col++) {
-                randomState = CellState.DEAD;
-                double rand = Math.random();
-                if(rand > 0.5 && row != 0 && col != 0 && row != cells.length-1 && col != cells[0].length -1){
-                    randomState = CellState.ALIVE;
-                }
-                int x = col * CELL_SIZE;
-                int y = row * CELL_SIZE;
-                cells[row][col] = new Cell(x, y, CELL_SIZE, row, col, randomState, rules);
-            }
-        }*/
+        x = 0;
+        top = data.size() - 1;
+    }
+    public void draw() {
+        background(255);
+        for (int i = 0; i < data.size(); i++){
+            data.get(i).display();
+            fill(0);
+        }
+        Main.app.textSize(20);
+        text("Instructions", 680, 240);
+        Main.app.textSize(14);
+        text(input, 933, 280);
+        text("1. Click 2 to sort", 650, 260);
+        text("2. Enter a song title in lowercase and click enter: ", 650, 280);
+        text("3. Click 3 to search for your song! ", 650, 300);
+        text("4. Click 4 to reset!", 650, 320);
+        if(found == true){
+            text("found at index " + middle, 690, 150);
+        }else if(notExist == true){
+            text("does not exist", 690, 150);
+        }else if(notYet == true){
+            text("not found yet", 690, 150);
+        }
     }
 
-    private int binarySearchIterative(){
-        int middle = (top+bottom)/2;
+    private int binarySearchIterative() {
+        middle = (top + bottom) / 2;
         Song current = data.get(middle);
-        if(target.compareTo(current) == 0){
+        if (target.compareTo(current) == 0) {
             return middle;
-        }else if(target.compareTo(current) == 1){
-            top = middle+1;
+        } else if (target.compareTo(current) == -1) {
+            top = middle - 1;
             return top;
-        }else{
-            bottom = middle-1;
+        } else {
+            bottom = middle + 1;
             return bottom;
         }
     }
 
-    public void keyPressed(){
-        if (key == 'a'){
-            binarySearchIterative();
+    private void selectionSort() {
+        for (int curIndex = 0; curIndex < data.size() - 1; curIndex++) {
+            int minIndex = findMin(curIndex);
+            swap(curIndex, minIndex);
+        }
+        for(int i = 0; i<data.size(); i++){
         }
     }
-
-    public void assignXY(){
-        for(int i = 0; i< data.size(); i++){
-            if(i == 4 || i == 9 || i == 14){
-                x = 40;
-                y += 120;
-                data.get(i).setX(x);
-                data.get(i).setY(y);
-            }else{
-                x+= 120;
-                data.get(i).setX(x);
+    private int findMin(int startingIndex) {
+        int minIndex = startingIndex;
+        for (int i = minIndex + 1; i < data.size(); i++) {
+            if (data.get(i).compareTo(data.get(minIndex)) == -1) {
+                minIndex = i;
             }
+        }
+        return minIndex;
+    }
+    private void swap( int a, int b) {
+        Song temp = data.get(a);
+        data.set(a, data.get(b));
+        data.set(b, temp);
+        int tempx = data.get(a).getX();
+        data.get(a).setX(data.get(b).getX());
+        data.get(b).setX(tempx);
+    }
+
+    private int binarySearchRecursive(int target, int bottom, int top){
+        if (bottom <= top) {
+            int middle = (bottom+top)/2;
+            if ((data.get(middle).getiD())  > target)
+            {
+                binarySearchRecursive(target, bottom, middle - 1);
+            }
+            else if ((data.get(middle).getiD()) < target)
+            {
+                binarySearchRecursive(target, middle+1, top);
+            }
+            else
+            {
+                return middle;
+            }
+        }
+        return -1;
+    }
+    public void reset(){
+        Table table = loadTable("data/songData.csv", "header");
+        for (TableRow row : table.rows()) {
+            int iD =  row.getInt("iD"); // obtain iD
+            String songName = row.getString("Name"); // obtain song name
+            String artist = row.getString("Artist"); // obtain artist
+            int streams = row.getInt("streams"); // obtain artist
+            data.add(new Song(iD, songName, artist, streams, x, y, 255));
+            x+=110;
+        }
+        x = 0;
+        top = data.size() - 1;
+    }
+
+    public void keyPressed() {
+        if(key == '2'){
+           selectionSort();
+        }
+        if(key != '2' && key != '3' && key!= ENTER && key != RETURN && key != '4') {
+            input += key;
+        }if(key == RETURN || key == ENTER){
+            if (input.equals("seven")) {
+                target = data.get(0); //assigning target to data at index of "seven" (not creating a new Song object for target)
+            } else if (input.equals("lala")) {
+                target = data.get(1);
+            } else if (input.equals("vampire")) {
+                target = data.get(2);
+            } else if (input.equals("cruel summer")) {
+                target = data.get(3);
+            } else if (input.equals("where she goes")) {
+                target = data.get(4);
+            } else if (input.equals("sprinter")) {
+                target = data.get(5);
+            } else if (input.equals("ella baila sola")) {
+                target = data.get(6);
+            } else if (input.equals("columbia")) {
+                target = data.get(7);
+            } else if (input.equals("fukumean")) {
+                target = data.get(8);
+            } else if (input.equals("la bebe")) {
+                target = data.get(9);
+            } else if (input.equals("un x100to")) {
+                target = data.get(10);
+            } else if (input.equals("super shy")) {
+                target = data.get(11);
+            } else if (input.equals("flowers")) {
+                target = data.get(12);
+            }else{
+                notExist = true;
+            }
+        } if(key == '3' && notExist == false) {
+            data.get(middle).changeColor(255);
+            if (binarySearchIterative() == middle) {
+                data.get(middle).changeColor(0);
+                found = true;
+            } else if (middle == 0 || middle == data.size()-1) {
+                data.get(middle).changeColor(0);
+            } else {
+                data.get(middle).changeColor(0);
+                notYet = true;
+            }
+        }if(key == '4'){
+            x = 20;
+            y = 20;
+            bottom = 0;
+            top = data.size()-1;
+            found = false;
+            notExist = false;
+            notYet = false;
+            data.clear();
+            reset();
+            input = "";
         }
     }
 }
--- src/Song.java
+++ src/Song.java
@@ -3,53 +3,50 @@ public class Song implements Comparable{
     private String artist;
     private int streams;
     private int iD;
-    private int x, y, size, index;
+    private int x, y;
     private int fillColor;
 
-    public Song(int iD, String songName, String artist, int streams, int x, int y, int size, int index, int fillColor){
+    public Song(int iD, String songName, String artist, int streams, int x, int y,  int fillColor){
         this.songName = songName;
         this.artist = artist;
         this.streams = streams;
         this.iD = iD;
         this.x = x;
         this.y = y;
-        this.size = size;
-        this.index = index;
         this.fillColor = fillColor;
-
+    }
+    public Song(int iD){
+        this.iD = iD;
     }
     public void display(){
-        Main.app.fill(fillColor);
-        Main.app.rect(x, y, 80, 80);
+        Main.app.textSize(13);
+        Main.app.fill(fillColor, 200, 250);
+        Main.app.rect(x, y, 100, 100);
+        Main.app.fill(0);
+        Main.app.text("iD " + iD, x + 3, y + 20);
+        Main.app.text(streams, x +3, y + 40);
+        Main.app.text(artist, x+3, y + 60);
+        Main.app.text(songName, x+3, y + 80);
     }
-
     @Override
     public int compareTo(Object anotherObject){
-        Song y = (Song) anotherObject;
-        if (this.streams < ((Song) anotherObject).streams){
+        if (this.iD < ((Song) anotherObject).iD){
             return -1;
-        } else if (this.streams > ((Song)anotherObject).streams){
+        } else if (this.iD > ((Song)anotherObject).iD){
             return 1;
         } else {
             return 0;
         }
     }
-    public int getStreams(){
-        return streams;
+    public int getiD(){
+        return iD;
     }
-    public void changeColor(){
-       fillColor = 255;
-    }
-    public int getFillColor(){
-        return fillColor;
-    }
-
     public void setX(int x){
         this.x = x;
     }
-    public void setY(int y){
-        this.y = y;
+    public int getX(){return x;}
+    public void changeColor(int color){
+       fillColor = color;
     }
-
 }
 
